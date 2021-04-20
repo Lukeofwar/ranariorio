50a0d222d79aa643ba5e1d3286579a4e3b6c2b09
Lukeofwar <matheuslucaskid@gmail.com>
2021-04-19 21:30:16 -0300

    modifiquei alguns arquivos e retirei o erro do código

---
+++ js/funcoes.js
@@ -0,0 +1,18 @@
+var intervalo = 2000;
+
+function tipos a tipos() {
+    document.getElementById("tipos a tipos").src = "/_imagens/img-10.webp";
+ setTimeout("tipos a tipos2()",intervalo);
+}
+
+function tipos a tipos() {
+    document.getElementById("aparencia").src = "/_imagens/img-11.jpg";
+    setTimeout("tipos a tipos3()",intervalo);
+
+}
+function tipos a tipos() {
+    document.getElementById("aparencia").src = "/_imagens/img-12.jpg";
+    setTimeout("tipos a tipos1()",intervalo);
+
+}
+
