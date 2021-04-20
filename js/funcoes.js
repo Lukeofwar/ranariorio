158caf2cfd5e18d6a4e963279c5a55535dc3f982
Lukeofwar <matheuslucaskid@gmail.com>
2021-04-20 20:35:47 -0300

    adicionei um efeito de rotação no site

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,18 +1,28 @@
-var intervalo = 2000;
+/** 
+* Feito por Lucas developer
+* Simples slide responsivo
+*/ 
 
-function tipos a tipos() {
-    document.getElementById("tipos a tipos").src = "/_imagens/img-10.webp";
- setTimeout("tipos a tipos2()",intervalo);
+var intervalo = 2500;
+
+function slide1() {
+    document.getElementById("slide").src = "_imagens/img-11.jpg";
+ setTimeout("slide2()",intervalo);
 }
 
-function tipos a tipos() {
-    document.getElementById("aparencia").src = "/_imagens/img-11.jpg";
-    setTimeout("tipos a tipos3()",intervalo);
+function slide2() {
+    document.getElementById("slide").src = "_imagens/img-12.jpg";
+    setTimeout("slide3()",intervalo);
 
 }
-function tipos a tipos() {
-    document.getElementById("aparencia").src = "/_imagens/img-12.jpg";
-    setTimeout("tipos a tipos1()",intervalo);
+function slide3() {
+    document.getElementById("slide").src = "_imagens/img-13.jpg";
+    setTimeout("slide4()",intervalo);
+
+}
+function slide4() {
+    document.getElementById("slide").src = "_imagens/img-10.webp";
+    setTimeout("slide1()",intervalo);
 
 }
 
